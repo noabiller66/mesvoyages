1ebdda285f6070e593e2cda27931157449ce78b3
Bille <Bille@Noa.bts.sio>
2024-10-07 11:35:56 +0200

    Phase 3 : utilisation de Twig.

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AccueilController extends AbstractController{
+    /**
+     * @Route("/", name="accueil")
+     * @return Reponse
+     */
+    public function index(): Response{
+    
+        return $this->render('pages/accueil.html.twig');
+    }
+}
